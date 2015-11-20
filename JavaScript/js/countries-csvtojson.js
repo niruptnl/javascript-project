88c29d8183f8ce85d8108700ad0a3ce0a1dbba29
unknown <NA324237@L-777726102.wipro.com>
2015-11-20 14:15:48 +0530

    firstjson file

---
+++ JavaScript/js/countries-csvtojson.js
@@ -0,0 +1,28 @@
+fs = require('fs')
+var result = [];
+
+var data=fs.readFileSync('../countrylist.csv', 'utf8');
+var first=function (data) {
+
+  var lines=data.trim().split("\n");
+
+  var headers=lines[0].trim().split(",");
+
+  for(var i=1;i<lines.length;i++){
+	  var obj = {};
+	  var currentline=lines[i].trim().split(",");
+		obj[currentline[0]] = currentline[1];
+	  result.push(obj);
+  }
+  //  console.log(JSON.stringify(result,null,4)); //JSON
+
+   fs.writeFile("countrieslist.json", JSON.stringify(result, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to countrieslist.json");
+    }
+});
+};
+
+first(data);
---
+++ JavaScript/js/total-csvtojson.js
@@ -0,0 +1,35 @@
+fs = require('fs')
+fs.readFile('../total.csv', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  else{
+
+
+  var lines=data.trim().split("\n");
+
+  var result = [];
+  var headers=lines[0].trim().split(",");
+
+  for(var i=1;i<lines.length;i++){
+	  var obj = {};
+	  var currentline=lines[i].trim().split(",");
+
+    for(var j=1;j<headers.length-2;j++){
+		  obj[headers[j]] = currentline[j];
+	  }
+
+	  result.push(obj);
+  }
+  //  console.log(JSON.stringify(result,null,4)); //JSON
+
+   fs.writeFile("total.json", JSON.stringify(result, null, 4), function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to total.json");
+    }
+});
+      }
+
+});
